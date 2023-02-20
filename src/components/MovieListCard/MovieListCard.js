d5f698de08212a98e013cefe9a5156eb0f0a0f8f
martari03 <rishnyakmarta@gmail.com>
2023-02-20 10:09:34 +0200

    project

---
+++ src/components/MovieListCard/MovieListCard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MovieListCard = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export {MovieListCard};
---
+++ src/configs/urls.js
@@ -0,0 +1,7 @@
+const baseURL = 'https://api.themoviedb.org/3';
+
+const apiKey = '9ffdf592dfeedc324faf03dcfbe17428';
+
+const urls = {};
+
+export {baseURL, apiKey, urls};
